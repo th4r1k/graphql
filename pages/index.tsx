fa54811d0557050324ac171c0ee29ae9f55aa985
th4r1k <th4r1k@gmail.com>
2022-07-01 08:43:36 -0300

    fix: Improved responsivity

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,24 +25,23 @@ const Home: NextPage = () => {
     router.push("/Event")
   
   }
-    
 
   return (
     <>
-     <div className="h-screen overflow-hidden bg-blur bg-cover bg-no-repeat flex flex-col items-center ">
-      <div className="w-full max-w-[1100px] flex flex-col-reverse items-center justify-between mt-20 mx-auto sm:flex-row">
+     <div className="overflow-y-hidden bg-blur bg-cover bg-no-repeat flex flex-col items-center ">
+      <div className="w-full max-w-[1100px] flex flex-col items-center justify-between mt-20 mx-auto sm:flex-row">
         <div className="max-w-[640px]">
 
           <h1 className="mt-8 text-[2.5rem] leading-tight text-center">
             Learn some <strong className="text-blue-500">Code</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed mx-3">
+          <p className="mt-4 text-gray-200 leading-relaxed mx-3 mb-2">
             Here you can find awesome videos to improve your code, all videos were selected from Youtube...
             This Form is just a code pratice your data will not be used, after submit you will be redirected to the content.
           </p>
         </div>
           
-      <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+      <div className="p-8 bg-gray-700 border border-gray-500 rounded flex-shrink-0">
         <strong className="text-2xl mb-6 block">Subscribe! It is Free... </strong>
           
           <form onSubmit={handleSubscribe} action="" className="w-full flex flex-col gap-2" >
